867c909c08e10c3f6a4032b398c611ee25056fe7
Steph34 <spodar.s@protonmail.com>
2021-10-27 09:39:31 +0000

    initialisation repo

---
+++ main.js
@@ -0,0 +1,17 @@
+// déclaration des variables :
+let a;
+let b;
+let r;
+
+// inialisation des variables :
+
+a = 10;
+b = 20;
+
+// opérations arithmétiques :
+
+
+
+// affichage du résultat (valeur de la variable r) :
+
+console.log(r);
