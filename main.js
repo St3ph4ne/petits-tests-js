a7d17279a9a3994c3a24330a8df4b9700c445613
Steph34 <spodar.s@protonmail.com>
2021-10-28 09:05:47 +0000

    structure else if

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
 // déclaration des variables :
-let a;
-let b;
-let r;
+let a = 10;
+let b = 20;
 
-// inialisation des variables :
+console.log("a = " + a);
+console.log("b = " + b);
+// structures de controle : Les conditions :
+/**
+ * Dès qu'une condition est remplie, il n'exécute pas les autres
+ */
 
-a = 10;
-b = 20;
+if(a === b) {
+    // ici le code est exécuté si a égal b.
+    console.log("a est égal à b.");
+} else if (a > b) {
+    // si a n'est pas égal à b et si a>b exécute le code ici.
+    console.log("a est supérieur à b.");
+} else if (a < b) {
+    // si a n'est pas égal à b et si a<b exécute le code ici.
+    console.log("a est inférieur à b.");
+}else if (a >= b) {
+    // si a n'est pas égal à b et si a est supérieur ou égal à b
+    // mais le code ne sera pas exécuté car le 1er if '(a===b)' a été validé.
+    console.log("a est supérieur ou égal à b.");
+} else if (a <= b) {
+    // si a n'est pas égal à b et si a est inférieur ou égal à b
+    // mais le code ne sera pas exécuté car le 1er if '(a===b)'a été validé.
+    console.log("a est inférieur ou égal à b.");
+} else if (a != b) {
+    // si a n'est pas égal à b et si a est différent de b. hihi
+    // mais n'exécute pas ce code car un premier else if est validé avant
+    // le else if (a<b)
+    // il serait exécuté si ce 'else if' était placé avant le 'else if (a<b)'
+        console.log("a n'est pas égal à b, tout en étant différent de b.");
+        console.log("hihi");
+}
 
-// opérations arithmétiques :
 
 
 
-// affichage du résultat (valeur de la variable r) :
 
-console.log(r);
+
